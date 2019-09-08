4ba67a37efa278521b99823ca148bef3274489ff
Alexander Kraus <nr4@z10.info>
2019-09-08 17:05:31 +0200

    added initial files

---
+++ cs.c
@@ -0,0 +1,23 @@
+/* congenial-spoon - Automatic VJ tool by Team210
+ * Copyright (C) 2019 Alexander Kraus <nr4@z10.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "Windows.h"
+
+int WINAPI demo(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    return 0;
+}
