757cb663aeea75aa9da576f4bab5add23600a1fc
Alexander Kraus <nr4@z10.info>
2019-09-08 19:48:06 +0200

    rudimentary rendering code

--- cs.c
+++ cs.c
@@ -15,9 +15,409 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "Windows.h"
+#include <windows.h>
+#include <Windowsx.h>
+#include <commctrl.h>
+#include <mmsystem.h>
+#include <Mmreg.h>
+#include <vfw.h>
+
+#include <GL/gl.h>
+#include <glext.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *demoname = "congenial-spoon";
+int w = 1280, h = 720;
+// Supported resolutions
+const int nresolutions = 9;
+const char *resolution_names[] = 
+{
+    // 16:9
+    "1920*1080",
+    "1600*900",
+    "1280*720",
+    "960*540",
+    // 4:3
+    "1600*1200",
+    "1280*960",
+    "1024*768",
+    "800*600",
+    "640*480"
+};
+const int widths[] = 
+{
+    1920,
+    1600,
+    1280,
+    960,
+    1600,
+    1280,
+    1024,
+    800,
+    640
+};
+const int heights[] = 
+{
+    1080,
+    900,
+    720,
+    540,
+    1200,
+    960,
+    768,
+    600,
+    480
+};
+HDC hdc;
+HGLRC glrc;
+float t_start, t_now;
+
+// OpenGL extensions
+PFNGLGETPROGRAMIVPROC glGetProgramiv;
+PFNGLGETSHADERIVPROC glGetShaderiv;
+PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog;
+PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
+PFNGLCREATESHADERPROC glCreateShader;
+PFNGLCREATEPROGRAMPROC glCreateProgram;
+PFNGLSHADERSOURCEPROC glShaderSource;
+PFNGLCOMPILESHADERPROC glCompileShader;
+PFNGLATTACHSHADERPROC glAttachShader;
+PFNGLLINKPROGRAMPROC glLinkProgram;
+PFNGLUSEPROGRAMPROC glUseProgram;
+PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation;
+PFNGLUNIFORM2FPROC glUniform2f;
+PFNGLUNIFORM1FPROC glUniform1f;
+PFNGLGENFRAMEBUFFERSPROC glGenFramebuffers;
+PFNGLBINDFRAMEBUFFERPROC glBindFramebuffer;
+PFNGLFRAMEBUFFERTEXTURE2DPROC glFramebufferTexture2D;
+PFNGLNAMEDRENDERBUFFERSTORAGEEXTPROC glNamedRenderbufferStorageEXT;
+PFNGLUNIFORM1IPROC glUniform1i;
+#ifdef WIN32
+PFNGLACTIVETEXTUREPROC glActiveTexture;
+#endif
+
+int flip_buffers()
+{
+	SwapBuffers(hdc);
+
+	MSG msg = { 0 };
+	while ( PeekMessageA( &msg, NULL, 0, 0, PM_REMOVE ) )
+	{
+		if ( msg.message == WM_QUIT ) {
+			return FALSE;
+		}
+		TranslateMessage( &msg );
+		DispatchMessageA( &msg );
+	}
+
+	return TRUE;
+}
+
+void quad()
+{
+    glBegin(GL_QUADS);
+    glVertex3f(-1,-1,0);
+    glVertex3f(-1,1,0);
+    glVertex3f(1,1,0);
+    glVertex3f(1,-1,0);
+    glEnd();
+    glFlush();
+}
+
+void draw()
+{
+//     glBindFramebuffer(GL_FRAMEBUFFER, first_pass_framebuffer);
+    
+    // update render time! TODO
+    
+    // render first pass! - by using a program. TODO
+
+    quad();
+
+    // Render post processing to buffer
+//     glUseProgram(post_program);
+//     glUniform2f(post_iResolution_location, w, h);
+//     glUniform1f(post_iFSAA_location, fsaa);
+//     glUniform1i(post_iChannel0_location, 0);
+//     glUniform1f(post_iTime_location, t-11);
+//     
+//     glActiveTexture(GL_TEXTURE0);
+//     glBindTexture(GL_TEXTURE_2D, first_pass_texture);
+// //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+// 
+//     quad();
+//     
+//     // Render to screen
+//     glBindFramebuffer(GL_FRAMEBUFFER, 0);
+//     
+//     glUseProgram(text_program);
+//     glUniform2f(text_iResolution_location, w, h);
+//     glUniform1f(text_iFontWidth_location, font_texture_size);
+//     glUniform1f(text_iTime_location, t-11);
+//     glUniform1i(text_iChannel0_location, 0);
+//     glUniform1i(text_iFont_location, 1);
+//     glUniform1f(text_iFSAA_location, fsaa);
+//     
+// #ifdef MIDI
+//     glUniform1f(text_iFader0_location, fader0);
+//     glUniform1f(text_iFader1_location, fader1);
+//     glUniform1f(text_iFader2_location, fader2);
+//     glUniform1f(text_iFader3_location, fader3);
+//     glUniform1f(text_iFader4_location, fader4);
+//     glUniform1f(text_iFader5_location, fader5);
+//     glUniform1f(text_iFader6_location, fader6);
+//     glUniform1f(text_iFader7_location, fader7);
+// #endif
+//     
+//     glActiveTexture(GL_TEXTURE0);
+//     glBindTexture(GL_TEXTURE_2D, first_pass_texture);
+// //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+//     
+//     glActiveTexture(GL_TEXTURE1);
+//     glBindTexture(GL_TEXTURE_2D, font_texture_handle);
+// //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, font_texture_size, font_texture_size, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+//     
+//     quad();
+//     glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uMsg)
+	{
+		case WM_KEYDOWN:
+			switch(wParam)
+			{
+				case VK_ESCAPE:
+					ExitProcess(0);
+					break;
+			}
+			break;
+        case WM_SYSCOMMAND:
+            switch(wParam)
+            {
+                case SC_SCREENSAVE:
+                case SC_MONITORPOWER:   
+                    return 0;           
+                    break;
+                    
+                default:
+                    break;
+            }
+            break;
+        
+		default:
+			break;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+LRESULT CALLBACK DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uMsg)
+	{
+		case WM_COMMAND:
+			UINT id =  LOWORD(wParam);
+			HWND hSender = (HWND)lParam;
+
+			switch(id)
+			{
+				case 5:
+				{
+					int index = SendMessage(hSender, CB_GETCURSEL, 0, 0);
+					w = widths[index];
+                    h = heights[index];
+				}
+                break;
+                case 7:
+					DestroyWindow(hwnd);
+					PostQuitMessage(0);
+                break;
+			}
+			break;
+
+		case WM_CLOSE:
+			ExitProcess(0);
+			break;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 
 int WINAPI demo(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    return 0;
+    AllocConsole();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+    
+    // Display settings selector
+	WNDCLASS wca = { 0 };
+	wca.lpfnWndProc   = DialogProc;
+	wca.hInstance     = hInstance;
+	wca.lpszClassName = L"Settings";
+	RegisterClass(&wca);
+	HWND lwnd = CreateWindowEx(
+		0,                              // Optional window styles.
+		L"Settings",                     // Window class
+		demoname,    // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style
+
+		// Size and position
+		200, 200, 300, 360,
+
+		NULL,       // Parent window
+		NULL,       // Menu
+		hInstance,  // Instance handle
+		NULL        // Additional application data
+		);
+
+	// Add "Resolution: " text
+	HWND hResolutionText = CreateWindow(WC_STATIC, "Resolution: ", WS_VISIBLE | WS_CHILD | SS_LEFT, 10,15,100,100, lwnd, NULL, hInstance, NULL);
+
+	// Add resolution Combo box
+	HWND hResolutionComboBox = CreateWindow(WC_COMBOBOX, TEXT(""),
+	 CBS_DROPDOWN | CBS_HASSTRINGS | WS_CHILD | WS_OVERLAPPED | WS_VISIBLE,
+	 100, 10, 175, 400, lwnd, (HMENU)5, hInstance,
+	 NULL);
+    
+    // Add items to resolution combo box and select full HD
+    for(int i=0; i<nresolutions; ++i)
+        SendMessage(hResolutionComboBox, (UINT) CB_ADDSTRING, (WPARAM) 0, (LPARAM) resolution_names[i]);
+	SendMessage(hResolutionComboBox, CB_SETCURSEL, 2, 0);
+    w = widths[2];
+    h = heights[2];
+    
+    // Add start button
+	HWND hwndButton = CreateWindow(WC_BUTTON,"Offend!",WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,185,225,90,90,lwnd,(HMENU)7,hInstance,NULL);
+    
+    // Show the selector
+	ShowWindow(lwnd, TRUE);
+	UpdateWindow(lwnd);
+    
+    MSG msg = { 0 };
+	while(GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	
+	// Display demo window
+	CHAR WindowClass[]  = "Team210 Demo Window";
+
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
+	wc.lpfnWndProc = &WindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = NULL;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = WindowClass;
+	wc.hIconSm = NULL;
+
+	RegisterClassEx(&wc);
+
+	// Create the window.
+    HWND hwnd = CreateWindowEx(
+            0,                                                          // Optional window styles.
+            WindowClass,                                                // Window class
+            ":: Team210 :: GO - MAKE A DEMO ::",                                 // Window text
+            WS_POPUP | WS_VISIBLE,                                      // Window style
+            0,
+            0,
+            w,
+            h,                     // Size and position
+
+            NULL,                                                       // Parent window
+            NULL,                                                       // Menu
+            hInstance,                                                  // Instance handle
+            0                                                           // Additional application data
+        );
+    
+	DEVMODE dm = { 0 };
+    dm.dmSize = sizeof(dm);
+    dm.dmPelsWidth = w;
+    dm.dmPelsHeight = h;
+    dm.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT;
+    
+    ChangeDisplaySettings(&dm, CDS_FULLSCREEN);
+    
+	// Show it
+	ShowWindow(hwnd, TRUE);
+	UpdateWindow(hwnd);
+
+	// Create OpenGL context
+	PIXELFORMATDESCRIPTOR pfd =
+	{
+		sizeof(PIXELFORMATDESCRIPTOR),
+		1,
+		PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
+		PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
+		32,                   // Colordepth of the framebuffer.
+		0, 0, 0, 0, 0, 0,
+		0,
+		0,
+		0,
+		0, 0, 0, 0,
+		24,                   // Number of bits for the depthbuffer
+		8,                    // Number of bits for the stencilbuffer
+		0,                    // Number of Aux buffers in the framebuffer.
+		PFD_MAIN_PLANE,
+		0,
+		0, 0, 0
+	};
+
+	hdc = GetDC(hwnd);
+
+	int  pf = ChoosePixelFormat(hdc, &pfd);
+	SetPixelFormat(hdc, pf, &pfd);
+
+	glrc = wglCreateContext(hdc);
+	wglMakeCurrent(hdc, glrc);
+    
+    // OpenGL extensions
+	glGetProgramiv = (PFNGLGETPROGRAMIVPROC) wglGetProcAddress("glGetProgramiv");
+	glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC) wglGetProcAddress("glGetProgramInfoLog");
+	glGetShaderiv = (PFNGLGETSHADERIVPROC) wglGetProcAddress("glGetShaderiv");
+	glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC) wglGetProcAddress("glGetShaderInfoLog");
+	glCreateShader = (PFNGLCREATESHADERPROC) wglGetProcAddress("glCreateShader");
+	glCreateProgram = (PFNGLCREATEPROGRAMPROC) wglGetProcAddress("glCreateProgram");
+	glShaderSource = (PFNGLSHADERSOURCEPROC) wglGetProcAddress("glShaderSource");
+	glCompileShader = (PFNGLCOMPILESHADERPROC) wglGetProcAddress("glCompileShader");
+	glAttachShader = (PFNGLATTACHSHADERPROC) wglGetProcAddress("glAttachShader");
+	glLinkProgram = (PFNGLLINKPROGRAMPROC) wglGetProcAddress("glLinkProgram");
+	glUseProgram = (PFNGLUSEPROGRAMPROC) wglGetProcAddress("glUseProgram");
+	glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC) wglGetProcAddress("glGetUniformLocation");
+	glUniform2f = (PFNGLUNIFORM2FPROC) wglGetProcAddress("glUniform2f");
+	glUniform1f = (PFNGLUNIFORM1FPROC) wglGetProcAddress("glUniform1f");
+	glGenFramebuffers = (PFNGLGENFRAMEBUFFERSPROC) wglGetProcAddress("glGenFramebuffers");
+	glBindFramebuffer = (PFNGLBINDFRAMEBUFFERPROC) wglGetProcAddress("glBindFramebuffer");
+	glFramebufferTexture2D = (PFNGLFRAMEBUFFERTEXTURE2DPROC) wglGetProcAddress("glFramebufferTexture2D");
+	glNamedRenderbufferStorageEXT = (PFNGLNAMEDRENDERBUFFERSTORAGEEXTPROC) wglGetProcAddress("glNamedRenderbufferStorage");
+	glActiveTexture = (PFNGLACTIVETEXTUREPROC) wglGetProcAddress("glActiveTexture");
+	glUniform1i = (PFNGLUNIFORM1IPROC) wglGetProcAddress("glUniform1i");
+
+    ShowCursor(FALSE);
+    
+    // Main loop
+    __int64 current_time, cps;
+    QueryPerformanceCounter((LARGE_INTEGER*)&current_time);
+    QueryPerformanceFrequency((LARGE_INTEGER*)&cps);
+    t_start = (double)current_time/(double)cps;
+	
+	while(flip_buffers())
+	{
+        QueryPerformanceCounter((LARGE_INTEGER*)&current_time);
+        t_now = (double)current_time/(double)cps;
+        
+		draw();
+	}
+
+	return 0;
 }
